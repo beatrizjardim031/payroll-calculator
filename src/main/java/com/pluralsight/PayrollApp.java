181cb7742b455b685e7643759fad7e38ed0c147d
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-04-10 11:47:31 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/PayrollApp.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class PayrollApp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+/*  Create a Java application named payroll-calculator
+    that prompts the user to enter: */
+
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
+
+    }
+}
