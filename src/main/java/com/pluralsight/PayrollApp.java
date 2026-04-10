427c2f3a1a2664869e6b8a694a1e136100b7c290
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-04-10 13:20:37 -0500

    Feat: Add pay rate and calculates how much they will get paid per week

--- src/main/java/com/pluralsight/PayrollApp.java
+++ src/main/java/com/pluralsight/PayrollApp.java
@@ -9,8 +9,22 @@ public class PayrollApp {
 /*  Create a Java application named payroll-calculator
     that prompts the user to enter: */
 
+//  Ask for user to enter their name and projects into string variable
         System.out.println("Enter your name: ");
         String name = input.nextLine();
+
+/*  Ask for user to enter how many hours they worked and projects into double variable
+    I chose to use double in this case because float is too limited
+ */
+        System.out.println("How many hours per week did you work?");
+        double hoursWorked = input.nextDouble();
+
+//  Ask for user to input their pay rate
+        System.out.println("What is your pay rate?");
+        double payRate = input.nextDouble();
+
+//  Prints the person's name and how much they are getting paid per week
+        System.out.printf("%s, your gross pay per week is: $%.2f", name, (hoursWorked * payRate));
 
     }
 }
