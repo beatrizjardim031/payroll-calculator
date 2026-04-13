5e1c952453b291424e8209a69f60200eb00e1616
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-04-13 15:36:35 -0500

    Fix: Add method for hours worked and pay rate

--- src/main/java/com/pluralsight/PayrollApp.java
+++ src/main/java/com/pluralsight/PayrollApp.java
@@ -3,28 +3,30 @@ package com.pluralsight;
 import java.util.Scanner;
 
 public class PayrollApp {
+/*  Ask for user to enter how many hours they worked and projects into double variable
+I chose to use double in this case because float is too limited */
+
+    public static double hoursWorked (Scanner input) {
+        System.out.println("How many hours per week did you work?");
+        return input.nextDouble();
+
+    }
+//  Ask for user to input their pay rate
+    public static double payRate (Scanner input) {
+        System.out.println("What is your pay rate?");
+        return input.nextDouble();
+    }
+
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-/*  Create a Java application named payroll-calculator
-    that prompts the user to enter: */
 
 //  Ask for user to enter their name and projects into string variable
         System.out.println("Enter your name: ");
         String name = input.nextLine();
 
-/*  Ask for user to enter how many hours they worked and projects into double variable
-    I chose to use double in this case because float is too limited
- */
-        System.out.println("How many hours per week did you work?");
-        double hoursWorked = input.nextDouble();
-
-//  Ask for user to input their pay rate
-        System.out.println("What is your pay rate?");
-        double payRate = input.nextDouble();
-
 //  Prints the person's name and how much they are getting paid per week
-        System.out.printf("%s, your gross pay per week is: $%.2f", name, (hoursWorked * payRate));
+        System.out.printf("%s, your gross pay per week is: $%.2f", name, (hoursWorked(input) * payRate(input)));
 
     }
 }
